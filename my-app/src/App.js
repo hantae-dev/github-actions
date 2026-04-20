688f708b52102a26d61893e28be5b75263d4cba0
hantae <85547680+hantae96@users.noreply.github.com>
2026-04-20 12:17:18 +0900

    수정

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn GithubAction cicd 한태~!!
+          수정중~
         </a>
       </header>
     </div>
